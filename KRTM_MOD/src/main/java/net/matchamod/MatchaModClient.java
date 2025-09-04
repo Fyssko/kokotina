8d9ecc7ebe67e77c838d07ff2419c26e6747abf7
DougezOfficial <121669683+DougezOfficial@users.noreply.github.com>
2025-09-04 18:27:06 +0200

    Added Sources pt.1

---
+++ KRTM_MOD/src/main/java/net/matchamod/MatchaModClient.java
@@ -0,0 +1,10 @@
+package net.matchamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MatchaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
